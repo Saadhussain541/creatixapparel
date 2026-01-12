1ae5855e37e95a0226902c8aadc75e0c96b3649e
saadhussain <saadhussain5111@gmail.com>
2026-01-12 17:47:10 +0500

    third commit

--- creatixapparel/src/components/form/social-login.tsx
+++ creatixapparel/src/components/form/social-login.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import google from "@/assets/img/login/google.svg";
-import facebook from "@/assets/img/login/facebook.svg";
-import apple from "@/assets/img/login/apple.svg";
 import Link from "next/link";
 
 export default function SocialLogin() {
@@ -10,18 +7,18 @@ export default function SocialLogin() {
     <div className="tp-login-social mb-10 d-flex flex-wrap align-items-center justify-content-center">
       <div className="tp-login-option-item has-google">
         <Link href="#">
-          <Image src={google} alt="google" />
+          <span>G</span>
           Sign in with google
         </Link>
       </div>
       <div className="tp-login-option-item">
         <Link href="#">
-          <Image src={facebook} alt="facebook" />
+          <span>f</span>
         </Link>
       </div>
       <div className="tp-login-option-item">
         <Link href="#">
-          <Image src={apple} alt="apple" className="apple" />
+          <span>🍎</span>
         </Link>
       </div>
     </div>
