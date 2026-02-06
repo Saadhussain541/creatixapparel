172a602522878b2c338c56fa0cbfbc5adb4513d8
Saadhussain541 <saadhussain5111@gmail.com>
2026-02-07 01:45:12 +0500

    logoes add

--- src/components/brand/brand-slider.tsx
+++ src/components/brand/brand-slider.tsx
@@ -4,17 +4,20 @@ import Image from "next/image";
 import Marquee from "react-fast-marquee";
 
 // brand images
-import b_1 from "@/assets/img/home-01/brand/brand-1.png";
-import b_2 from "@/assets/img/home-01/brand/brand-2.png";
-import b_3 from "@/assets/img/home-01/brand/brand-3.png";
-import b_4 from "@/assets/img/home-01/brand/brand-4.png";
-import b_5 from "@/assets/img/home-01/brand/brand-5.png";
-import b_6 from "@/assets/img/home-01/brand/brand-6.png";
-import b_7 from "@/assets/img/home-01/brand/brand-7.png";
-import b_8 from "@/assets/img/home-01/brand/brand-2.png";
+import b_1 from "@/assets/img/home-04/software/1.png";
+
+import b_2 from "@/assets/img/home-04/software/2.png";
+
+import b_3 from "@/assets/img/home-04/software/3.png";
+
+import b_4 from "@/assets/img/home-04/software/4.png";
+
+import b_5 from "@/assets/img/home-04/software/5.png";
+
+import b_6 from "@/assets/img/home-04/software/5.png";
 
 
-const brand_images = [b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8,  b_1, b_2, b_3, b_4, b_5, b_6, b_7, b_8];
+const brand_images = [b_1, b_2, b_3, b_4, b_5, b_6,  b_1, b_2, b_3, b_4, b_5, b_6];
 
 export default function BrandSlider() {
   return (
